d005692765046beb7f3073997be7976181d0170b
Juan Pablo Prado <juan@juanpabloprado.com>
2018-07-09 22:16:35 -0500

    Log exception

--- src/main/java/KafkaProducerApp.java
+++ src/main/java/KafkaProducerApp.java
@@ -1,8 +1,12 @@
 import java.util.Properties;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class KafkaProducerApp {
+  private static final Logger logger = LoggerFactory.getLogger(KafkaProducerApp.class);
+
   public static void main(String[] args) {
     Properties props = new Properties();
     props.put("bootstrap.servers", "localhost:9092, localhost:9093");
@@ -14,7 +18,7 @@ public class KafkaProducerApp {
         myProducer.send(new ProducerRecord<>("my-topic", i + "", "MyMessage: " + i));
       }
     } catch (Exception e) {
-      e.printStackTrace();
+      logger.error("Failed to send kafka producer record", e);
     }
   }
 }
